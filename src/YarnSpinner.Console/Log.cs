71cf2f7248da659869d7274ea513877b5ea59a32
Jon Manning <jon@secretlab.com.au>
2021-10-10 09:17:34 +1100

    Log now supports printing Diagnostic objects

--- src/YarnSpinner.Console/Log.cs
+++ src/YarnSpinner.Console/Log.cs
@@ -29,10 +29,31 @@ namespace YarnSpinnerConsole
             PrintLine("🚨 ERROR", ConsoleColor.Red, text);
         }
 
+        [System.Diagnostics.CodeAnalysis.DoesNotReturn]
         public static void Fatal(string text)
         {
             PrintLine("🚨 ERROR", ConsoleColor.Red, text);
             Environment.Exit(1);
+        }
+
+        public static void Diagnostic(Yarn.Compiler.Diagnostic diagnostic)
+        {
+            var messagePrefix = string.IsNullOrEmpty(diagnostic.FileName) ? string.Empty : $"{diagnostic.FileName}: {diagnostic.Line}:{diagnostic.Column} ";
+
+            var message = messagePrefix + diagnostic.Message;
+
+            switch (diagnostic.Severity)
+            {
+                case Yarn.Compiler.Diagnostic.DiagnosticSeverity.Error:
+                    Error(message);
+                    break;
+                case Yarn.Compiler.Diagnostic.DiagnosticSeverity.Warning:
+                    Warn(message);
+                    break;
+                case Yarn.Compiler.Diagnostic.DiagnosticSeverity.Info:
+                    Info(message);
+                    break;
+            }
         }
     }
 }
