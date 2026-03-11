57456e2edf4fff1f95f9ebac54c351455bb65060
Jon Manning <jon@secretlab.com.au>
2026-03-11 19:11:01 +1100

    Add TimeTracker class

---
+++ src/YarnSpinner.Console/Shared/TimeTracker.cs
@@ -0,0 +1,46 @@
+#nullable enable
+
+namespace YarnSpinnerConsole
+{
+    using System;
+    using System.Diagnostics;
+
+    public class TimeTracker
+    {
+        public TimeTracker(System.Action<string, double, double> onLog)
+        {
+            this.OnLog = onLog;
+            this.Stopwatch = new System.Diagnostics.Stopwatch();
+            this.TotalStopwatch = new System.Diagnostics.Stopwatch();
+        }
+
+        private string? CurrentPhase { get; set; }
+
+        public void StartPhase(string phase)
+        {
+            if (CurrentPhase != null)
+            {
+                OnLog(CurrentPhase, Stopwatch.Elapsed.TotalSeconds, TotalStopwatch.Elapsed.TotalSeconds);
+            }
+            CurrentPhase = phase;
+            Stopwatch.Restart();
+            TotalStopwatch.Start();
+        }
+
+        public void Stop()
+        {
+            if (CurrentPhase != null)
+            {
+                OnLog(CurrentPhase, Stopwatch.Elapsed.TotalSeconds, TotalStopwatch.Elapsed.TotalSeconds);
+            }
+            CurrentPhase = null;
+            Stopwatch.Stop();
+            TotalStopwatch.Stop();
+        }
+
+
+        public Action<string, double, double> OnLog { get; }
+        public Stopwatch Stopwatch { get; }
+        public Stopwatch TotalStopwatch { get; }
+    }
+}
